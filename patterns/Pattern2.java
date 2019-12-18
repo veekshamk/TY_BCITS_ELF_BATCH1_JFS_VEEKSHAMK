0da0935d443f7894ecb2b314208dbebb0deea5f0
Veeksha M K <veekshakodandoor2010@gmail.com>
2019-12-18 16:37:52 +0530

    patterns

---
+++ patterns/Pattern2.java
@@ -0,0 +1,19 @@
+package org.bcits.patterns;
+import java.util.Scanner;
+public class Pattern2 
+{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the value of n:");
+	int n=sc.nextInt();
+	for(int i=n; i>0; i--)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
---
+++ patterns/Pattern6.java
@@ -0,0 +1,22 @@
+package org.bcits.patterns;
+import java.util.Scanner;
+public class Pattern6 
+{
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the value of n:");
+	int n=sc.nextInt();
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=1; j<=n-i; j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1; j<=2*i-1; j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
