e00e593d2222b5c08f090dc0fb39f9170db3cdc9
Veeksha M K <veekshakodandoor2010@gmail.com>
2019-12-24 14:36:38 +0530

    hacker rank

---
+++ HackerRank/src/com/hackerrank/datastructure/ArrayList1.java
@@ -0,0 +1,43 @@
+package com.hackerrank.datastructure;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayList1 {
+	public static void main(String[] args) {
+		System.out.println("enter the number of lines:");
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		ArrayList<ArrayList<Integer>> arrayLists=new ArrayList<ArrayList<Integer>>(n);
+		ArrayList<Integer> arrayList=null;
+		
+		for(int i=1; i<=n; i++)
+		{
+			System.out.println("enter the number of digits in "+ i+"th line");
+			int d=sc.nextInt();
+			arrayList= new ArrayList<Integer>(d);
+			arrayList.add(0,d);
+			
+			for(int j=1; j<=d; j++) {
+				System.out.println("enter the value in "+j+"th position");
+				arrayList.add(j, sc.nextInt());
+			}
+			System.out.println(arrayList.toString());
+			arrayLists.add(arrayList);
+		}
+	System.out.println(arrayLists.toString());
+	
+	System.out.println("enter the number of queries:");
+	int q=sc.nextInt();
+	
+	for(int i=1; i<=q; i++) {
+		System.out.println("enter the line number:");
+		int x=sc.nextInt();
+		System.out.println("enter the position:");
+		int y=sc.nextInt();
+		System.out.println(arrayLists.get(x-1).get(y-1));
+	}
+	}
+
+}
