71529a9ae32d5406a83607d2e5ab6f948d66fd64
Veeksha M K <veekshakodandoor2010@gmail.com>
2019-12-28 18:13:29 +0530

    jdbc

--- myjavaproject/src/com/abc/myjavaapp/common/MyJavaApplicationStarterProgram.java
+++ myjavaproject/src/com/abc/myjavaapp/common/MyJavaApplicationStarterProgram.java
@@ -4,10 +4,10 @@ import com.mycomp.calculator.basic.BasicCalculator;
 
 public class MyJavaApplicationStarterProgram {
 	public static void main(String[] args) {
+
 		BasicCalculator cal=new BasicCalculator();
 		int sum=cal.add(10,20);
 		System.out.println("sum==>"+sum);
-		
 	}
 
 }
